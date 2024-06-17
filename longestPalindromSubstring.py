8e40e116d024b69ff326ca00d4abdff8f24522a5
Chen Cen <arrow.cen@gmail.com>
2024-06-16 22:22:18 -0700

    some final dps

---
+++ longestPalindromSubstring.py
@@ -0,0 +1,44 @@
+from typing import List, Optional
+from listNode import ListNode
+from treeNode.treeNode import TreeNode
+
+
+# Given a string s, return the longest palindromic substring in s
+#
+# Example 1:
+#
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+# Example 2:
+#
+# Input: s = "cbbd"
+# Output: "bb
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i + 1]
+
+        # checking from [0, 2], [1, 3] .... because [0, 1] and [1, 2] ... already populated
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    ans = [i, j]
+
+        i, j = ans
+        return s[i : j + 1]
+
+
+if __name__ == '__main__':
+    print(Solution())
